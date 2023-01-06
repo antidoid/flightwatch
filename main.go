bfce23d3340641cb136192189da28590fb1cb3f8
Piyush Singh <singhpiyush998@gmail.com>
2023-01-06 16:11:41 +0530

    Setup Frontend and Backend routes

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"os"
 
+	"github.com/antidoid/fligthwatch/controllers"
 	"github.com/antidoid/fligthwatch/initializers"
 
 	"github.com/gofiber/fiber/v2"
+    "github.com/gofiber/template/html"
 )
 
 func init() {
@@ -13,12 +15,23 @@ func init() {
 }
 
 func main() {
-	app := fiber.New()
+    // Load templates
+    engine := html.New("./views", ".html")
 
-	app.Get("/", func(c *fiber.Ctx) error {
+    // Setup App
+	app := fiber.New(fiber.Config{
+        Views: engine,
+    })
+
+    // Backend Routes
+	app.Get("/api/", func(c *fiber.Ctx) error {
         return c.SendString("Hello, Universe!")
 	})
 
+    // Frontend Routes
+    app.Get("/", controllers.Home)
+
+    // Start the app
 	app.Listen(":" + os.Getenv("PORT"))
 }
 
